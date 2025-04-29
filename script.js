d9939abf56845f5598f2ebad651c6711ad5769e1
mubanga-1 <mubanga.s10@gmail.com>
2025-04-29 13:01:03 -0700

    Implement console version of program
    Created and implemented version of program that is only acessible in the
    console. Also ensures to add to major components of the program that are
    essential for the program's functionality.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const ticTacToe = (function() {
     const board = [];
+    const signatures = ["X", "O"];
+
     for (let i = 0; i < 9; i++) {
         board.push(" ");
     }
+
     const turns = 9;
 
     const printBoard = () => {
@@ -11,11 +14,84 @@ const ticTacToe = (function() {
         }
     }
 
-    const makeMove = (player, position) => {
-        board[position - 1] = player.letter;
+    const availableMoves = () => {
+        const moves = [];
+        let counter = 0;
+
+        for (space of board) {
+            if (!signatures.includes(space)) {
+                moves.push(counter);
+            }
+            counter++;
+        }
+
+        return moves;
+    }
+    
+    const makeMove = (player, space) => {
+        const position = space - 1;
+        if (availableMoves().includes(position)) {
+            board[position] = player.letter;
+            return true;
+        }
+        return false;
     }
 
-    return {turns, printBoard, makeMove};
+    const winner = () => {
+        const {x, o} = signatures;
+        // Check rows
+        rows = [];
+        for (let i = 0; i < 3; i++) {
+            rows.push(board.slice(i * 3, (i + 1) * 3));
+        }
+
+        for (row of rows) {
+            if (row.every(item => item == x)) {
+                return x; 
+            } else if (row.every(item => item == o)) {
+                return o;
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (board[i] == x && board[i+3] == x && board[i+6] == x) {
+                return x;
+            } else if (board[i] == o && board[i+3] == o && board[i+6] == o) {
+                return o;
+            } 
+        }
+
+        // Check diagonals
+        firstDiagonalIndices = [0, 4, 8];
+        firstDiagonal = [];
+        for (index of firstDiagonalIndices) {
+            firstDiagonal.push(board[index])
+        }
+        
+        if (firstDiagonal.every(item => item == x)) {
+            return x;
+        } else if (firstDiagonal.every(item => item == o)) {
+            return o;
+        }
+
+        secondDiagonalIndices = [2, 4, 6];
+        secondDiagonal = [];
+        for (index of secondDiagonalIndices) {
+            secondDiagonal.push(board[index])
+            
+        }
+
+        if (secondDiagonal.every(item => item == x)) {
+            return x;
+        } else if (secondDiagonal.every(item => item == o)) {
+            return o;
+        }
+
+        return "";
+    }
+
+    return {turns, printBoard, makeMove, winner, availableMoves};
 })();
 
 
@@ -28,3 +104,45 @@ function createPlayer(letter) {
 
     return {letter};
 }
+
+
+console.log("Let's play TicTacToe!");
+const player1 = createPlayer("X");
+const player2 = createPlayer("O"); 
+const input = require("prompt-sync")();
+
+
+while (ticTacToe.turns > 0) {
+    ticTacToe.printBoard();
+    console.log();
+
+    console.log(ticTacToe.availableMoves());
+    
+
+    if (ticTacToe.turns % 2 == 0) {
+        if (ticTacToe.makeMove(player2, parseInt(input("O's turn: ")))) {
+            ticTacToe.turns--;
+        } else {
+            console.log("Invalid move!");
+        }
+    } else {
+        if (ticTacToe.makeMove(player1, parseInt(input("X's turn: ")))) {
+            ticTacToe.turns--;
+        } else {
+            console.log("Invalid move");
+        }
+    }
+
+    if (ticTacToe.winner()) {
+        ticTacToe.printBoard();
+        console.log();
+        console.log(ticTacToe.winner(), "wins!");
+        
+        break
+    }
+
+}
+    
+// const randomComputerPlayer = (function() {
+//     const {letter} = createPlayer()
+// })();
