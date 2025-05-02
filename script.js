37314cf5a7bce32bed1ac196ffcf46a93bf2f7d1
mubanga-1 <mubanga.s10@gmail.com>
2025-05-02 04:05:11 -0700

    Repurpose the program to work with a graphical ui
    Repurposed the program to work with a graphical version of the game in
    the browser. Replaced the functionality of printing a board in the
    console with one of generating a graphical board on the screen and
    ensuring that each space therein is displays the marker of a player when
    clicked.
    
    Implemented a mode where human plays other human with each it being made
    known who's turn it is to play each round via graphical marker atop the
    game's board. Additionally Implemented the notion of a reset button
    whereby the players can reset the game to the begin.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+const boardContainer = document.querySelector(".board");
+
 const ticTacToe = (function() {
     const board = [];
+
     // Populates the board with 9 empty spaces
     for (let i = 0; i < 9; i++) {
         board.push(" ");
     }
 
     const signatures = ["X", "O"];    
-    const turns = 9;
+    let turns = 9;
 
-    // Prints a grid like structure on which the each player's move are displayed
-    const printBoard = () => {
-        for (let i = 0; i < 3; i++) {
-            console.log("| " + board.slice(i * 3, (i + 1) * 3).join(" | ") + " |");
+    // Generates board on screen
+    const generateBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            const space = document.createElement("div");
+            space.className = "box";
+            space.id = i;
+            boardContainer.appendChild(space);
         }
     }
 
@@ -31,8 +37,7 @@ const ticTacToe = (function() {
     }
     
     // Acts as a mechanism via which players make different moves
-    const makeMove = (player, space) => {
-        const position = space - 1;
+    const makeMove = (player, position) => {
         if (availableMoves().includes(position)) {
             board[position] = player.letter;
             return true;
@@ -101,11 +106,18 @@ const ticTacToe = (function() {
         return "";
     }
 
-    return {turns, signatures, printBoard, makeMove, winner, availableMoves};
+    // Resets to the board to it's default state with 9 empty spaces
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = " ";
+        }
+    }
+
+    return {turns, signatures, generateBoard, makeMove, winner, availableMoves, resetBoard};
 })();
 
 
-const input = require("prompt-sync")();
+// const input = require("prompt-sync")();
 
 // Creates a human player object when called
 function humanPlayer(letter) {
@@ -116,74 +128,122 @@ function humanPlayer(letter) {
     }
 
     // Allows player to select which move to play
-    const getMove = () => {
-        console.log("Enter number (1-9)");
-        return parseInt(input(`${letter}'s turn: `));
+    const getMove = (position) => {
+        return parseInt(position.id);
     }
+
 
     return {letter, getMove};
 }
 
-// Creates a computer player when called
-const randomComputerPlayer = function(letter) {
-    
-    // Genereates a move for computer to make via the length of the list of available moves
-    const getMove = () => {
-        const move = Math.ceil((Math.random() * ticTacToe.availableMoves().length) + 1); 
-        console.log(`${letter} makes move on ${move}`);
-        return move;
-    }
-    return {letter, getMove}
-};
 
+// setting buttons
+const twoPlayerBtn = document.querySelector("#two-player");
+const onePlayerBtn = document.querySelector("#one-player");
+const clearBtn = document.querySelector("#clear-btn");
+const winner = document.querySelector(".winner");
 
-// Run through each round of the game until a player wins or a tie is had
-function playGame() {
-    console.log("Let's play TicTacToe!");
+// Player markers
+const playerOneMarker = document.querySelector(".turns #player-x");
+const playerTwoMarker = document.querySelector(".turns #player-o");
 
-    // Create players
-    const human = humanPlayer(input("Pick a letter X or O: ").trim().toUpperCase());
-    let computer;
-    human.letter == "X" ? computer = randomComputerPlayer("O") : computer = randomComputerPlayer("X");
-
-    // Assign playerOne and playerTwo based on which letter the uses chooses
-    let playerOne;
-    let playerTwo;
-
-    human.letter == "X" ? playerOne = human : playerOne = computer;
-    computer.letter == "O" ? playerTwo = computer : playerTwo = human;
-
-    // Iterates until a player wins or a tie is had
-    while (ticTacToe.turns > 0) {
-        ticTacToe.printBoard();
-        console.log();
-    
-        // Switch between playerOne and playerTwo
-        if (ticTacToe.turns % 2 == 0) {
-            if (ticTacToe.makeMove(playerTwo, playerTwo.getMove())) {
-                ticTacToe.turns--;
-            } else {
-                console.log("Invalid move!");
-            }
-        } else {
-            if (ticTacToe.makeMove(playerOne, playerOne.getMove())) {
-                ticTacToe.turns--;
-            } else {
-                console.log("Invalid move");
-            }
-        }
-
-        // Check if either player has won
-        if (ticTacToe.winner()) {
-            ticTacToe.printBoard();
-            console.log();
-            console.log(ticTacToe.winner(), "wins!");
-            
-            break
-        }
-    
-    }
-    
+// Switches between player markers to visually indicate which player's turn it is
+function switchPlayerMarker(markerOne, markerTwo) {
+    const secondStyling = markerTwo.style.borderBottom;
+    markerTwo.style.borderBottom = markerOne.style.borderBottom;
+    markerOne.style.borderBottom = secondStyling;
 }
 
-playGame()
+
+// // Creates a computer player when called
+// const randomComputerPlayer = function(letter) {
+    
+//     // Genereates a move for computer to make via the length of the list of available moves
+//     const getMove = () => {
+//         const move = Math.ceil((Math.random() * ticTacToe.availableMoves().length) + 1); 
+//         console.log(`${letter} makes move on ${move}`);
+//         return move;
+//     }
+//     return {letter, getMove}
+// };
+
+ticTacToe.generateBoard();
+
+// Changes the a button when give "h" as a flag and resets the color properties when give "r"
+function modifyColor(button, flag) {
+    if (flag == "h") {
+        button.style.backgroundColor = "black";
+        button.style.color = "white";              
+    } 
+
+    if (flag == "r") {
+        button.style.backgroundColor = "";
+        button.style.color = "";              
+    }
+}
+
+// Is called when the user wishes to player against another human
+function playHuman(event) {
+    const playerOne = humanPlayer("X");
+    const playerTwo = humanPlayer("O");
+
+    const target = event.target;
+
+    // Switch between playerOne and playerTwo
+    if (ticTacToe.turns % 2 == 0) {    
+        switchPlayerMarker(playerOneMarker, playerTwoMarker);
+
+        if (ticTacToe.makeMove(playerTwo, playerTwo.getMove(target))) {
+            target.innerText = playerTwo.letter;
+            ticTacToe.turns--;
+        } 
+
+    } else {        
+        switchPlayerMarker(playerOneMarker, playerTwoMarker);
+
+        if (ticTacToe.makeMove(playerOne, playerOne.getMove(target))) {
+            target.innerText = playerOne.letter;
+            ticTacToe.turns--;
+        }
+    } 
+
+    // Displays winner if there is one
+    let isWinner = ticTacToe.winner();
+    if (isWinner) {
+        winner.innerText = `${isWinner}'s wins!`;   
+        modifyColor(twoPlayerBtn, "r");
+    } 
+
+    // Informations the players that it's a tie if there is no winner
+    if (ticTacToe.turns == 0) {
+        winner.innerText = "It's a tie!"         
+        modifyColor(twoPlayerBtn, "r");
+    } 
+} 
+
+// Adds the functionality of completely resetting the game if the user clicks the clear board function
+clearBtn.addEventListener("click", () => {
+    const spaces = document.querySelectorAll(".box");
+    spaces.forEach((space) => {
+        space.innerText = "";
+    });
+
+    ticTacToe.turns = 9;
+    ticTacToe.resetBoard();
+    winner.innerText = "";
+
+    playerOneMarker.style.borderBottom = ".25rem solid transparent";
+    playerTwoMarker.style.borderBottom = ".25rem solid transparent";
+
+
+    boardContainer.removeEventListener("click", playHuman);  
+});
+
+
+// Sets up the game for two human players when the twoPlayerBtn button is clicked 
+twoPlayerBtn.addEventListener("click", () => {
+    modifyColor(twoPlayerBtn, "h");
+    playerOneMarker.style.borderBottom = ".25rem solid green";
+    boardContainer.addEventListener("click", playHuman);
+});
+
